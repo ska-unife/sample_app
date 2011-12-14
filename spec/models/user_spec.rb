c179ec1d0af4ca7cbf347ca9dff6da9e6d7728d4
Luigi Scagnet <luigi.scagnet@gmail.com>
2011-12-14 12:34:55 +0100

    Inseriti i test per validazione password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-
 # == Schema Information
 #
 # Table name: users
@@ -12,66 +11,88 @@ require 'spec_helper'
 #  updated_at :datetime
 #
 
-
 describe User do
-    before(:each) do
-        @attr = { :name => "Example user", :email => "example@ilpescecane.it"}
-    end
+  before(:each) do
+    @attr = { 
+      :name => "Example user", 
+      :email => "example@ilpescecane.it",
+      :password => "foobar",
+      :password_confirmation => "foobar"
+    }
+  end
 
-    it "Crea nuovo utente" do
-        User.create!(@attr)
-    end
+  it "Crea nuovo utente" do
+    User.create!(@attr)
+  end
 
-    it "Should require a name" do
-        no_name_user = User.new(@attr.merge(:name => ""))
-        no_name_user.should_not be_valid
-    end
+  it "Nome necessario" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
 
-    it "Should require an email" do
-        no_email_user = User.new(@attr.merge(:email => ""))
-        no_email_user.should_not be_valid
+  it "E-mail necessaria" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+    
+  it "Errore nome troppo lungo" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end    
+
+  it "Mail accettata" do
+    addresses = %w[info@ilpescecane.it pluto@gmail.com cane@cucia.org]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+    
+  it "Mail non accettata" do
+    addresses = %w[info_at_ilpescecane.it pluto@gmail,com cane@cucia.org.]
+    addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
+    
+  it "Mail deve essere unica, no duplicazioni" do
+    User.create!(@attr)
+    user_with_duplicate_mail = User.new(@attr)
+    user_with_duplicate_mail.should_not be_valid 
+  end
+    
+  it "Mail deve essere unica, no duplicazioni, e case insensitive pippo@pc.it = PIPPo@pC.It" do
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_mail = User.new(@attr)
+    user_with_duplicate_mail.should_not be_valid 
+  end
+
+  describe "Validazione password" do
+    it "Password vuota" do
+      no_password_user = User.new(@attr.merge(:password => "", :password_confirmation => ""))
+      no_password_user.should_not be_valid
+    end
+  
+    it "Password e conferma non uguali" do
+      no_password_valid = User.new(@attr.merge( :password_confirmation => "invalid"))
+      no_password_valid.should_not be_valid
     end
     
-    it "Errore nome troppo lungo" do
-        long_name = "a" * 51
-        long_name_user = User.new(@attr.merge(:name => long_name))
-        long_name_user.should_not be_valid
-    end    
-
-    it "Mail accettata" do
-        addresses = %w[info@ilpescecane.it pluto@gmail.com cane@cucia.org]
-        addresses.each do |address|
-            valid_email_user = User.new(@attr.merge(:email => address))
-            valid_email_user.should be_valid
-        end
+    it "Password troppo corta" do
+      short = "a" * 5
+      short_password = User.new(@attr.merge( :password => short, :password_confirmation => short ))
+      short_password.should_not be_valid
     end
-    
-    it "Mail non accettata" do
-        addresses = %w[info_at_ilpescecane.it pluto@gmail,com cane@cucia.org.]
-        addresses.each do |address|
-            invalid_email_user = User.new(@attr.merge(:email => address))
-            invalid_email_user.should_not be_valid
-        end
+  
+    it "Password troppo lunga" do
+      long = "a" * 41
+      long_password = User.new(@attr.merge( :password => long, :password_confirmation => long ))
+      long_password.should_not be_valid
     end
-    
-    it "Mail deve essere unica, no duplicazioni" do
-        User.create!(@attr)
-        user_with_duplicate_mail = User.new(@attr)
-        user_with_duplicate_mail.should_not be_valid 
-    end
-    
-    it "Mail deve essere unica, no duplicazioni, e case insensitive pippo@pc.it = PIPPo@pC.It" do
-        upcased_email = @attr[:email].upcase
-        User.create!(@attr.merge(:email => upcased_email))
-        user_with_duplicate_mail = User.new(@attr)
-        user_with_duplicate_mail.should_not be_valid 
-    end
- 
-
-
-
-
-
-
+  
+  end
 end
 
