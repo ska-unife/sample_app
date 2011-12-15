c524d72ecc77459e11036495acf0d4de6a88811a
Luigi Scagnet <luigi.scagnet@gmail.com>
2011-12-15 18:22:41 +0100

    Creata controller sessione, iniziato sistema login

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 SampleApp::Application.routes.draw do
-    
-    get "users/new"
-    
-    resources :users
 
-    root :to => 'pages#home'
-  
-    match '/about', :to => 'pages#about'
-    match '/contact', :to => 'pages#contact'
-    match '/help', :to => 'pages#help'
-    
-    match '/signup', :to => 'users#new'
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+  root :to => "pages#home"
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
 
 # The priority is based upon order of creation:
   # first created -> highest priority.
