0e18942b97f580b1e8fbe8c1fc3947f434d95ff5
Luigi Scagnet <luigi.scagnet@gmail.com>
2011-12-17 18:03:17 +0100

    al lavoro

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,7 @@ module SessionsHelper
     cookies.permanent.signed[:remember_token] = user.id, user.salt
     current_user = user
   end
+  def current_user=(user)
+    @current_user = user
+  end
 end
